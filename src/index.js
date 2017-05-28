8147872d616f4aebddf427b85ff26ed65e577409
Charles Burgess <consulting@cvburgess.com>
2017-05-28 10:58:43 -0400

    add dependencies and organize files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const apiUrl = process.env.NODE_ENV === "development"
+  ? 'http://localhost:3002/graphql'
+  : 'https://unscripted-cagematch-prod.herokuapp.com/graphql';
+
+const networkInterface = createNetworkInterface({ uri: apiUrl });
+
+// networkInterface.use([{
+//   applyMiddleware(req, next) {
+//     if (!req.options.headers) {
+//       req.options.headers = {};
+//     }
+//     const creds = localStorage.getItem('creds');
+//     req.options.headers.authorization = creds ? `Basic ${creds}` : null;
+//     next();
+//   }
+// }]);
+//
+// networkInterface.useAfter([{
+//   applyAfterware({ response }, next) {
+//     if (response.status === 401) {
+//       logout();
+//     }
+//     next();
+//   }
+// }]);
+
+const client = new ApolloClient({
+  dataIdFromObject: (result) => {
+    if (result.id && result.__typename) {
+      return result.__typename + result.id;
+    }
+    return null;
+  },
+  networkInterface
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
